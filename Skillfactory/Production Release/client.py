2407bf1b64eaa7205c5ee94fe7619d134bad1793
tgorbunov <gtimur2014@gmail.com>
2026-03-06 18:28:58 +0530

    update

--- Skillfactory/Production Release/client.py
+++ Skillfactory/Production Release/client.py
@@ -2,14 +2,14 @@ import requests
 
 if __name__ == '__main__':
     # выполняем POST-запрос на сервер по эндпоинту add с параметром json
-    r = requests.post('http://localhost:5000/add', json={'num': 5})
+    r = requests.post('http://localhost:5000/predict', json=[1, 0, 1, 24])
     # выводим статус запроса
-    print(r.status_code)
+    print('Status code: {}'.format(r.status_code))
     # реализуем обработку результата
     if r.status_code == 200:
         # если запрос выполнен успешно (код обработки=200),
         # выводим результат на экран
-        print(r.json()['result'])
+        print('Prediction: {}'.format(r.json()['prediction']))
     else:
         # если запрос завершён с кодом, отличным от 200,
         # выводим содержимое ответа
